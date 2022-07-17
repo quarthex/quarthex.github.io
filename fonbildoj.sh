3bb53898faeec9c12c3c03923336e78cffb86570
quarthex <quarthex@users.noreply.github.com>
2022-07-17 20:33:42 +0000

    deploy: d07182cfbbddc391e8341f67cb38ef35ded0c394

--- fonbildoj.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -ex
-convert -resize 1431X768    hela.webp    hela-mobile.webp
-convert -resize 1431X768 malhela.webp malhela-mobile.webp
--- ĉefpaĝo/karuselo.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-for n in 1 2 3 4 5 6
-do
-    convert -resize  910X512 karuselo-$n.webp karuselo-$n-tablet.webp 
-    convert -resize 1081X608 karuselo-$n.webp karuselo-$n-desktop.webp 
-    convert -resize 1252X704 karuselo-$n.webp karuselo-$n-widescreen.webp 
-done
